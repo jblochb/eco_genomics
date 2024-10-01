b9116f48d5c92697be5a223c6fc34c337acb8ac2
John Michael Blochberger <jblochbe@node321.cluster>
2024-10-01 17:22:21 -0400

    Coding for admixture analysis

--- population_genomics/docs/03_PCA_Admixture.R
+++ population_genomics/docs/03_PCA_Admixture.R
@@ -60,4 +60,73 @@ CentPCA$eigenvalues[1]/sum(CentPCA$eigenvalues)
 #Attempts to groups mixture into # of groups (K) (start with 1, up to maybe 10) which the researcher chooses
 #Algorithm assigns each individual to a group, calculate allele freq, calc 2pq, repeat until best result
 #Individual might belong to more than 1 group, Q = fractional ancestry
-     
+
+library(tidyverse)
+library(vcfR)
+library(SNPfiltR)
+library(LEA)
+vcf<- read.vcfR("outputs/vcf_final.filtered.vcf.gz")
+meta <- read.csv("/gpfs1/cl/pbio3990/PopulationGenomics/metadata/meta4vcf.csv")
+#must subset the meta final data 
+
+meta2 <- meta[meta$id %in% colnames(vcf@gt[,-1]),]
+
+# Now we will run admixture analyses and create admixture plots
+#For admixture, we use the LEA R package
+#The function inside LEA is called "snmf" (much faster running than Structure)
+
+CentAdmix <- snmf("outputs/vcf_final.filtered.thinned.geno",
+                  K = 1:10,
+                  entropy = T,
+                  repetitions = 3,
+                  project = "new",) #if you're adding to this analysis later, you could choose project = "continue"
+plot(CentAdmix, color = "blue4", main = "SNMF")
+#this plots the cross-entropy score we can use to select models with K values that fit our data well
+#investigate where the plot begins to level off but not fully (where the elbow is) in this case: K=3-5
+#the relative difference when you add K values is what is significant, you also want to select a less complex model (lower K)
+#K = 1:10 means evaluate each K value from 1-10
+#entropy = True asks it to test the model 
+#The project is saved into :outputs/vcf_final.filtered.thinned.snmfProject 
+
+#To load the project, use:
+  #project = load.snmfProject("outputs/vcf_final.filtered.thinned.snmfProject")
+
+#To remove the project, use:
+  #remove.snmfProject("outputs/vcf_final.filtered.thinned.snmfProject")
+par(mfrow=c(2,1))
+plot(CentPCA$eigenvalues[1:10], ylab = "Eigenvalues", xlab = "Number of PCs", color= "blue4", main = "PCA")
+plot(CentAdmix, col = "blue4", main = "SNMF")
+dev.off()
+myK=3
+#use to avoid changing in many different places in the future
+
+CE = cross.entropy(CentAdmix, K=myK)
+CE
+best = which.min(CE)
+
+myKQ = Q(CentAdmix, myK, run = best)
+#This Q score is % ancestry in each K group
+myKQmeta = cbind(myKQ, meta2)
+#cbind can combine two data frames if their rows are identical 
+my.colors= c("blue4", "darkolivegreen3", "tomato", "darkorchid", "darkgoldenrod1")
+
+myKQmeta = as_tibble(myKQmeta) %>%
+  group_by(continent) %>% 
+  arrange(region, pop, .by_group = TRUE)
+#first group separetly by continent then from within continent by region, within region by group
+#tibble is a tidyverse data table
+barplot(as.matrix(t(myKQmeta[ , 1:myK])),
+        border = NA,
+        space = 0,
+        col = my.colors[1:myK],
+        xlab = "Geographic Regions",
+        ylab = "Ancestry Proportions",
+        main = paste0("Ancestry Matrix K=",myK))
+axis(1,
+     at= 1:length(myKQmeta$region), # 1 means mess with x axis instead of y axis
+     tick = F,
+     labels=myKQmeta$region,
+     cex.axis = 0.5, #changes the size of labels
+     las = 3) #makes the labels go onto their sides 
+pdf("figures/Admixture_K3.pdf")
+dev.off()
---
+++ population_genomics/docs/04_selection.R
@@ -0,0 +1,74 @@
+library(tidyverse)
+library(vcfR)
+library(ggplot2)
+library(qqman)
+library(pcadapt)
+
+vcf<- read.pcadapt("/gpfs1/cl/pbio3990/PopulationGenomics/variants/vcf_final.filtered.vcf",
+                   type = "vcf")
+
+#vcfR<- read.vcfR("~/projects/eco_genomics/population_genomics/outputs/vcf_final.filtered.vcf.gz")
+vcfR <- read.vcfR("/gpfs1/cl/pbio3990/PopulationGenomics/variants/vcf_final.filtered.vcf.gz")
+meta<- read.csv("/gpfs1/cl/pbio3990/PopulationGenomics/metadata/meta4vcf.csv")
+meta2 <- meta[meta$id %in% colnames(vcfR@gt[,-1]),]
+
+pcadapt.pca <- pcadapt(vcf,
+                       K=2,
+                       method = "componentwise", #to test for selection separately on each axis
+                       min.maf = 0.01, #this will filter out the minor freq alleles
+                       LD.clumping = list(size =500, thr =0.2)) #PC axes will be thinned to only include non LD SNPs but still test for every SNP in the dataset
+summary(pcadapt.pca)
+#pass = # of loci that passed the LD filter
+plot(pcadapt.pca, options= "scores",
+     pop=meta2$region,
+     i = 1,
+     j = 2, K = 2)
+?plot.pcadapt
+#organize data to allow to visualize by chromosome
+view(head(vcfR@fix))
+
+vcfR.fix <- as.data.frame(vcfR@fix[,1:2])
+
+chr.main <- unique(vcfR.fix$CHROM)[1:8]
+chrnum <- as.data.frame(cbind(chr.main, seq(1,8,1)))
+
+pcadapt.pca$pvalues
+#NA values in this dataframe were filtered out with the min allele freq
+Pval <- pcadapt.pca$pvalues
+pcadapt.MHplot <- cbind(vcfR.fix, Pval)
+pcadapt.MHplot <- left_join(chrnum, pcadapt.MHplot, join_by(chr.main==CHROM))
+
+pcadapt.MHplot <- pcadapt.MHplot %>% 
+  mutate(SNP=paste0(chr.main,"_",POS))
+view(pcadapt.MHplot)
+pcadapt.MHplot$V2 = as.numeric(pcadapt.MHplot$V2)
+pcadapt.MHplot$POS = as.numeric(pcadapt.MHplot$POS)
+pcadapt.MHplot$pPC1 = as.numeric(pcadapt.MHplot[,4])
+pcadapt.MHplot$pPC2 = as.numeric(pcadapt.MHplot[,5])
+
+#manhattan plot will crash if there are any NA values
+pcadapt.MHplot <- pcadapt.MHplot %>% drop_na(pPC1)
+
+manhattan(pcadapt.MHplot,
+          chr = "V2",
+          bp = "POS",
+          p="pPC1",
+          col = c("blue4", "orange1"),
+          logp = T,
+          ylab = "-log10 p-value",
+          genomewideline = F,
+          main ="PCAdapt genome scan for selection (PC1)")
+manhattan(pcadapt.MHplot,
+          chr = "V2",
+          bp = "POS",
+          p="pPC2",
+          col = c("blue4", "orange1"),
+          logp = T,
+          ylab = "-log10 p-value",
+          genomewideline = F,
+          main ="PCAdapt genome scan for selection (PC2)")
+
+view(pcadapt.MHplot %>% 
+       filter(pPC1<quantile(pcadapt.MHplot$pPC1, 0.001)) %>% 
+       select(chr.main, POS, pPC1))
+hist(pcadapt.pca$maf)
